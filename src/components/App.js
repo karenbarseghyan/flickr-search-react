8af163d2115989cdd4107a10a598c9dc9b8f610c
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-03-19 13:54:36 +0400

    Creating basic layout api components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import Search from "./Search";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <p>Hello Task</p>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
